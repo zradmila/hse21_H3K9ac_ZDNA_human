24881de9dde59a21feb112f7f4dacbaf6e76e88c
zradmila <zaikinaradmila@gmail.com>
2021-05-23 16:00:44 +0300

    filter_peaks.R were added

---
+++ src/filter_peaks.R
@@ -0,0 +1,38 @@
+
+source('lib.R')
+
+###
+
+NAME <- 'H3K4me3_A549.ENCFF573MUH.hg19'
+#NAME <- 'H3K4me3_A549.ENCFF832EOL.hg19'
+
+###
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.init.hist.pdf'), path = OUT_DIR)
+
+# Remove long peaks
+bed_df <- bed_df %>%
+  arrange(-len) %>%
+  filter(len < 5000)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('filter_peaks.', NAME, '.filtered.hist.pdf'), path = OUT_DIR)
+
+bed_df %>%
+  select(-len) %>%
+  write.table(file=paste0(DATA_DIR, NAME ,'.filtered.bed'),
+              col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
