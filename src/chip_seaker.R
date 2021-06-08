960102866afbc7f7cbaf3c7ac4f67240ee36526d
zradmila <zaikinaradmila@gmail.com>
2021-06-08 21:47:39 +0300

    chip-seeker ZDNA

--- src/chip_seaker.R
+++ src/chip_seaker.R
@@ -8,15 +8,20 @@ source('lib.R')
 # BiocManager::install("TxDb.Hsapiens.UCSC.hg19.knownGene")
 # BiocManager::install("TxDb.Mmusculus.UCSC.mm10.knownGene")
 
+# BiocManager::install("clusterProfiler")
+
 library(ChIPseeker)
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 #library(TxDb.Mmusculus.UCSC.mm10.knownGene)
 library(clusterProfiler)
 
+#BiocManager::install("org.Hs.eg.db")
+
 ###
 
-NAME <- 'H3K4me3_A549.ENCFF573MUH.hg19.filtered'
-#NAME <- 'H3K4me3_A549.ENCFF832EOL.hg19.filtered'
+#NAME <- 'H3K9ac_K562.ENCFF148UQI.hg19.filtered'
+#NAME <- 'H3K9ac_K562.ENCFF891CHI.hg19.filtered'
+NAME <- 'DeepZ'
 BED_FN <- paste0(DATA_DIR, NAME, '.bed')
 
 ###
@@ -29,7 +34,7 @@ pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
 plotAnnoPie(peakAnno)
 dev.off()
 
-peak <- readPeakFile(BED_FN)
-pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
-covplot(peak, weightCol="V5")
-dev.off()
+# peak <- readPeakFile(BED_FN)
+# pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+# covplot(peak, weightCol="V5")
+# dev.off()
